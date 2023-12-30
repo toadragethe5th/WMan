3cfc009dfdf77a09e431b6a329bddb3b6521f5ad
ToadRage <153384304+toadragethe5th@users.noreply.github.com>
2023-12-30 09:29:01 -0500

    Update wman.bat

--- src/commands/wman.bat
+++ src/commands/wman.bat
@@ -1,7 +1,19 @@
 @echo off
 
+cd ~
+
 if "%1"=="" (
     echo ERROR: No argument
 ) else (
-    type repo.yml | find %1
+    for /F "delims=" %%i in ('type repo.yml ^| findstr "%1"') do (
+        set "output=%%i"
+    )
+
+    if "%output%" == "" (
+        echo ERROR: No package by that name.
+    ) else (
+        bitsadmin /create /download package
+        bitsadmin /addfile package "%output%"
+        bitsadmin /info package /verbose
+    )
 )
